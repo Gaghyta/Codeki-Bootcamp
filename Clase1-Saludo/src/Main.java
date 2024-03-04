b36752f02efd8271caab06865922aca76874e4a3
Carla Antonini <carla.antonini@hotmail.com>
2024-03-04 13:16:33 -0300

    CA clases 1 y 2 en modulos

---
+++ Clase1-Saludo/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Almacenar el nombre en una variable
+        String nombre = "Carla Antonini";
+
+        // Imprimir el saludo con el nombre almacenado en la variable
+        System.out.println("Hola mi nombre es " + nombre);
+    }
+}
