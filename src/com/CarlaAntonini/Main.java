638b85ddbd0b52ff5da0b1801ed0326d91d32d59
Carla Antonini <carla.antonini@hotmail.com>
2024-03-04 12:49:33 -0300

    CA commit inicial clases1 del Bootcamp Codeki

---
+++ src/com/CarlaAntonini/Main.java
@@ -0,0 +1,11 @@
+package com.CarlaAntonini;
+
+public class Main {
+    public static void main(String[] args) {
+        // Almacenar el nombre en una variable
+        String nombre = "Carla Antonini";
+
+        // Imprimir el saludo con el nombre almacenado en la variable
+        System.out.println("Hola mi nombre es " + nombre);
+    }
+}
