78567258774ce2173a195df151aff628b5d30fa4
Carla Antonini <carla.antonini@hotmail.com>
2024-03-04 13:24:40 -0300

    CA modifc en iml modules

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // Almacenar el nombre en una variable
-        String nombre = "Carla Antonini";
-
-        // Imprimir el saludo con el nombre almacenado en la variable
-        System.out.println("Hola mi nombre es " + nombre);
-    }
-}
